e4e9a058ed230eb2738eed608862ba9a963c3ae9
peterbandu527 <peterbarrios527@gmail.com>
2023-11-26 19:28:40 -0400

    actualizacion

--- src/components/logo_umc.tsx
+++ src/components/logo_umc.tsx
@@ -1,7 +1,9 @@
 const Logo_umc = () => {
   return (
-    <div className="container" style={{backgroundImage: `url(${`${import.meta.env.BASE_URL}`}/images/cabecera_1.png)`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', height: '150px'}}>
-      
+    <div className="container">
+      <img className="bg-white w-full" src={`${import.meta.env.BASE_URL}/images/cabecera_1.png`}
+        alt="Navbar logo" style={{maxWidth:'100%'}}>      
+      </img>
     </div>
   );
 };
